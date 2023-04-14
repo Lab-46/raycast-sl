63ced0c528b68c82198b3cd238b7b430270a1d5f
Albin Groen <albin.groen@gmail.com>
2023-04-14 13:25:20 +0200

    Update API url and command icon

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 import { Icon } from "@raycast/api";
 
-export const API_URL = "http://localhost:1337";
+export const API_URL = "https://sl-api.onrender.com";
 
 export const TRANSPORT_MODE_TO_ICON: Record<string, Icon> = {
   METRO: Icon.Train,
